e42830d45955d5cbad333ed9f6518cd618c871ac
James Hoctor <JEHoctor@protonmail.com>
2021-12-25 01:30:56 -0500

    Begin to separate dictionary load from dictionary search

--- spelling_bee/__init__.py
+++ spelling_bee/__init__.py
@@ -1,2 +1,3 @@
+import spelling_bee.dictionary as dictionary
 from spelling_bee.dictionary_search import DictionarySearch, dictionary_search
 from spelling_bee.nyt_scraping import HintData, PuzzleData
