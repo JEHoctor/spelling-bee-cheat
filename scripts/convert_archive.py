7d318c207883733fc85d333b38ebddd3c91c8ebd
James Hoctor <JEHoctor@protonmail.com>
2022-03-13 00:46:26 -0500

    Script to convert pickle archives to json
    You still have to remove the old pickle files yourself, if you want that

---
+++ scripts/convert_archive.py
@@ -0,0 +1,30 @@
+import click
+import pickle
+
+import spelling_bee
+
+
+@click.command()
+def convert_archive():
+    """
+    Convert the pickle files in the archive directory to json
+    """
+    archive = spelling_bee.archive.Archive()
+    pickle_files = sorted(archive.path.glob("*.pkl"))
+    for pf in pickle_files:
+        with pf.open("rb") as f:
+            puzzle = pickle.load(f)
+        tayp = spelling_bee.scrape.TodayAndYesterdayPuzzles.parse_raw(puzzle.game_data_json)
+        archive.archive(tayp.today)
+        archive.archive(tayp.yesterday)
+
+
+def main():
+    """
+    Main method. Call into click command
+    """
+    convert_archive()
+
+
+if __name__ == "__main__":
+    main()
