f2f4289b3025db6d60f841264e5bdaa1bf1f5a77
James Hoctor <JEHoctor@protonmail.com>
2023-05-16 17:36:17 -0400

    Ignore unused import in __init__.py files

--- src/spelling_bee_cheat/__init__.py
+++ src/spelling_bee_cheat/__init__.py
@@ -1,8 +1,8 @@
-import spelling_bee_cheat.archive as archive
-import spelling_bee_cheat.data_model as data_model
-import spelling_bee_cheat.dictionary as dictionary
-import spelling_bee_cheat.folders as folders
-import spelling_bee_cheat.markov_search as markov_search
-import spelling_bee_cheat.scrape as scrape
-from spelling_bee_cheat.dictionary_search import dictionary_search
-from spelling_bee_cheat.nyt_scraping import HintData, PuzzleData
+import spelling_bee_cheat.archive as archive  # noqa F401
+import spelling_bee_cheat.data_model as data_model  # noqa F401
+import spelling_bee_cheat.dictionary as dictionary  # noqa F401
+import spelling_bee_cheat.folders as folders  # noqa F401
+import spelling_bee_cheat.markov_search as markov_search  # noqa F401
+import spelling_bee_cheat.scrape as scrape  # noqa F401
+from spelling_bee_cheat.dictionary_search import dictionary_search  # noqa F401
+from spelling_bee_cheat.nyt_scraping import HintData, PuzzleData  # noqa F401
